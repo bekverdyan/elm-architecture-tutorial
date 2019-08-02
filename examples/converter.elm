4aabc965dcfdca2594eb7c640d90a3dda65ccea1
Aram Bekverdyan <aram.bekverdyan@gmail.com>
2019-08-02 14:12:27 +0400

    cleanup my changests

--- examples/converter.elm
+++ examples/converter.elm
@@ -1,5 +1,7 @@
+module Main exposing (Model, Msg(..), init, main, update, view, viewConverter)
+
 import Browser
-import Html exposing (Html, Attribute, span, input, text)
+import Html exposing (Attribute, Html, input, span, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
@@ -9,7 +11,7 @@ import Html.Events exposing (onInput)
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
 
 
 
@@ -17,13 +19,14 @@ main =
 
 
 type alias Model =
-  { input : String
-  }
+    { input : String
+    , converted : Float
+    }
 
 
 init : Model
 init =
-  { input = "" }
+    Model "" 0
 
 
 
@@ -31,14 +34,24 @@ init =
 
 
 type Msg
-  = Change String
+    = Change String
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Change newInput ->
-      { model | input = newInput }
+    case msg of
+        Change newInput ->
+            case String.toFloat newInput of
+                Just celsius ->
+                    { model
+                        | input = String.fromFloat celsius
+                        , converted = celsius * 1.8 + 32
+                    }
+
+                Nothing ->
+                    { model
+                        | converted = 0
+                    }
 
 
 
@@ -47,20 +60,19 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  case String.toFloat model.input of
-    Just celsius ->
-      viewConverter model.input "blue" (String.fromFloat (celsius * 1.8 + 32))
+    case String.toFloat model.input of
+        Just celsius ->
+            viewConverter model.input "blue" (String.fromFloat (celsius * 1.8 + 32))
 
-    Nothing ->
-      viewConverter model.input "red" "???"
+        Nothing ->
+            viewConverter model.input "red" "???"
 
 
 viewConverter : String -> String -> String -> Html Msg
 viewConverter userInput color equivalentTemp =
-  span []
-    [ input [ value userInput, onInput Change, style "width" "40px" ] []
-    , text "°C = "
-    , span [ style "color" color ] [ text equivalentTemp ]
-    , text "°F"
-    ]
-
+    span []
+        [ input [ value userInput, onInput Change, style "width" "40px" ] []
+        , text "°C = "
+        , span [ style "color" color ] [ text equivalentTemp ]
+        , text "°F"
+        ]
